6e788ff6b49473ddff4deb32eb76f5fa07a3b042
Rico Fedryan <63868992+vedrico14@users.noreply.github.com>
2024-08-20 15:38:05 +0700

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Update package list and install dependencies
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+# Add the GPG key for the official Docker repository to your system
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add the Docker repository to APT sources:
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+# Update the package list to include Docker packages
+sudo apt-get update
+
+# Install Docker Engine
+sudo apt install docker-ce
+
+# Output Docker version to verify installation
+docker --version
+
+echo "Docker installation completed successfully."
